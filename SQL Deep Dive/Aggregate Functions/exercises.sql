586153f0a62f5368bffe0161c25bf61eeecfa3b5
Manuel Torres <jmhernandez.nv@gmail.com>
2020-12-18 23:53:21 -0600

    Sesion 18-12
    Sesion 18-12-20 Advanced SQL

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,11 +8,11 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
-
+select avg(salary) from employees; -> 63810.744836143706
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+SELECT max(birth_date) FROM employees; -> 1965-02-01
 
 -- BEFORE YOU START
 /*
@@ -23,7 +23,8 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
+Mi respuesta: SELECT count("public"."towns"."name") FROM "public"."towns";
+Respuesta del curso: SELECT count(id) FROM towns;
 
 -- BEFORE YOU START
 /*
@@ -34,11 +35,23 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+Mi respuesta: SELECT count("language") FROM countrylanguage;
+
+Respuesta del curso: 
+select count(countrycode) from countrylanguage
+where isofficial = true;
+-- Output: 238
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
 
+select avg(lifeexpectancy) FROM country;
+-- Output: 66.48603611164265
+
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
 
+select AVG(population) from city
+where countrycode = 'NLD';
 
+--  Output: 185001.75
--- SQL Deep Dive/Group By/questions.sql
+++ SQL Deep Dive/Group By/questions.sql
@@ -4,16 +4,22 @@
 *  Table: Employees
 */
 
-SELECT e.emp_no
-FROM employees as e
+SELECT hire_date, COUNT(hire_date) as "amount"
+FROM employees
+GROUP BY hire_date
+ORDER BY "amount" DESC;
 
 /*
 *   Show me all the employees, hired after 1991 and count the amount of positions they've had
 *  Database: Employees
 */
 
-SELECT e.emp_no
+SELECT e.emp_no, e.hire_date, COUNT(t.title) as "amount of titles"
 FROM employees as e
+INNER JOIN titles as t ON t.emp_no = e.emp_no
+WHERE EXTRACT (year from hire_date) >= 1991
+GROUP BY e.emp_no
+ORDER BY e.emp_no;
 
 
 /*
@@ -21,5 +27,11 @@ FROM employees as e
 *  Database: Employees
 */
 
-SELECT e.emp_no
+SELECT e.emp_no, 
+       CONCAT(e.first_name, e.last_name) as "Name", 
+       de.dept_name 
 FROM employees as e
+JOIN dept_emp as d USING(emp_no)
+JOIn departments as de USING(dept_no)
+WHERE d.dept_no = 'd005'
+GROUP BY e.emp_no, de.dept_name ;
